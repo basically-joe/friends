e4361710ee54fc098d4be42f32a5229b037f60a5
Joe McColl <joe4kool@gmail.com>
2018-11-22 15:09:37 +0000

    No3 solved for true

--- friends.rb
+++ friends.rb
@@ -5,3 +5,8 @@ end
 def get_tv_show(person)
 return person[:favourites][:tv_show]
 end
+
+def fav_snacks(person, snacks)
+  fav_snacks = person[:favourites][:snacks]
+ return "true" if fav_snacks
+end
